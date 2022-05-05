d7b82f96208551fda9930bc80e4967582b572e41
Cristian Ponce <cristian.ponce@onephase.com>
2022-05-04 20:23:57 -0500

    1st commit

---
+++ precache-manifest.682efdf7aa7d8332af0956f72f4bc832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cc4e99b6e9d369653f148a948b963e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd0ae5d48e2bff08cbd",
+    "url": "/static/css/2.29e82c64.chunk.css"
+  },
+  {
+    "revision": "b96819ea1dedacae8461",
+    "url": "/static/css/main.0ace3b34.chunk.css"
+  },
+  {
+    "revision": "ddd0ae5d48e2bff08cbd",
+    "url": "/static/js/2.459213ad.chunk.js"
+  },
+  {
+    "revision": "a4adcac158c08d81ecd0dd3d0d8ff6d6",
+    "url": "/static/js/2.459213ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96819ea1dedacae8461",
+    "url": "/static/js/main.d3297959.chunk.js"
+  },
+  {
+    "revision": "5000c1b12c51c11f375f",
+    "url": "/static/js/runtime-main.a2cb156a.js"
+  }
+]);
